f36733b5d20e090acf5883b4d4e42f213519c07e
MyClass <anasala.merthy@gmail.com>
2022-10-07 11:58:30 +0200

    securiter part2

--- src/main/java/bxl/mapper/UtilisateurMapper.java
+++ src/main/java/bxl/mapper/UtilisateurMapper.java
@@ -2,7 +2,7 @@ package bxl.mapper;
 
 import bxl.model.dto.UtilisateurDTO;
 import bxl.model.entities.Utilisateur;
-import bxl.model.forms.UtilisateurCreeEleveForm;
+
 import bxl.model.forms.UtilisateurCreeForm;
 import bxl.model.forms.UtilisateurUpdateForm;
 import org.springframework.stereotype.Component;
@@ -24,25 +24,11 @@ public class UtilisateurMapper {
         Utilisateur user = new Utilisateur();
         user.setUsername(form.getUsername());
         user.setPassword(form.getPassword());
-        user.setRoles(form.getRoles());
+
         return user;
     }
 
-    public Utilisateur toEntity(UtilisateurCreeEleveForm form) {
-        Utilisateur user = new Utilisateur();
-        user.setUsername(form.getUsername());
-        user.setPassword(form.getPassword());
-        user.setRoles(form.getRoles());
-        return user;
-    }
 
-    public Utilisateur toEntity(UtilisateurCreeProfForm form) {
-        Utilisateur user = new Utilisateur();
-        user.setUsername(form.getUsername());
-        user.setPassword(form.getPassword());
-        user.setRoles(form.getRoles());
-        return user;
-    }
 
     public Utilisateur toEntity(UtilisateurUpdateForm form) {
         Utilisateur user = new Utilisateur();
