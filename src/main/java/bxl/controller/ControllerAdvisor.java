cadbe31e9c252034f4b88f53bdb02a352dee6933
MyClass <anasala.merthy@gmail.com>
2022-10-14 17:13:42 +0200

    commit final

--- src/main/java/bxl/controller/ControllerAdvisor.java
+++ src/main/java/bxl/controller/ControllerAdvisor.java
@@ -1,10 +1,14 @@
 package bxl.controller;
 
 
+import nonapi.io.github.classgraph.json.JSONUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
 public class ControllerAdvisor {
 
+    public static void main(String[] args) {
+        System.out.println("commit final");
+    }
 
 }
